e3b8a80ee1388e3e75656ff666669da83c14d04c
Jan Toth <jtoth@i4industry.eu>
2022-04-05 16:55:15 +0200

    Dorobit

--- SynopsisThatWontContainDogsForSure/MainPage.xaml.cs
+++ SynopsisThatWontContainDogsForSure/MainPage.xaml.cs
@@ -195,6 +195,27 @@ namespace SynopsisThatWontContainDogsForSure
             var endTime = startTime - DateTime.Now;
             OutputList.ItemsSource = outputModels;
         }
+
+        private void ParralelForPartitionedThatWorks()
+        {
+            // Source must be array or IList.
+            var source = Enumerable.Range(0, 100000).ToArray();
+
+            // Partition the entire source array.
+            var rangePartitioner = Partitioner.Create(0, source.Length);
+
+            double[] results = new double[source.Length];
+
+            // Loop over the partitions in parallel.
+            Parallel.ForEach(rangePartitioner, (range, loopState) =>
+            {
+                // Loop over each range element without a delegate invocation.
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    results[i] = source[i] * Math.PI;
+                }
+            });
+        }
         private void OperationBtn_Click(object sender, RoutedEventArgs e)
         {
             if (FirstTabBtn.IsChecked == true) SequentialFor(100000);
